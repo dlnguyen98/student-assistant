03f612b49441505a7b7fd8c759edf08cff52f8d4
Daniel Nguyen <dlnguyen98@gmail.com>
2024-11-19 23:57:29 -0800

    First update

---
+++ student-assistant/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import "./Navbar.css";
+
+function Navbar() {
+    return (
+        <>
+        <h1>About</h1>
+        <p>Hello there.<br />How do you do?</p>
+        </>
+    );
+}
+
+export default Navbar;
